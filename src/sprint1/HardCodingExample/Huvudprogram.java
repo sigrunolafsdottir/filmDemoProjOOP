dd38529a1b32af35551b62517e24cf89aa142a6b
sigrunolafsdottir <solafsdottir@gmail.com>
2022-10-01 13:39:20 +0200

    hard coding demo

--- src/sprint1/HardCodingExample/Huvudprogram.java
+++ src/sprint1/HardCodingExample/Huvudprogram.java
@@ -10,11 +10,16 @@ public class Huvudprogram {
 
     public Huvudprogram(){
 
+        //Att skriva namnet på nedanstående sätt kan, tekniskt sett, betraktas som hårdkodning.
+        //Detta tycker jag dock är ok i denna uppgift då inga krav har ställts på att ni ska
+        //läsa från fil eller skapa en "Lägg till ny växt"-funktion.
+        //I produktionsmässiga, storskapiga program brukar man inte skapa data på dtta sätt
+        //utan man läser in datat från en databas. Det kommer ni att få lära er i databas-kursen :)
         Kaktus igge = new Kaktus("Igge");
 
         String VäxtSOmSkaHaVätska = JOptionPane.showInputDialog("Vem ska ha vätska");
 
-        //Dubbellagra INTE namnet, läs istället från de instantierade objekten
+        //Dubbellagra INTE namnet, läs istället namnen från de instantierade objekten
         if (VäxtSOmSkaHaVätska.equals("Igge")){
             //Låt INTE info som hör hemma i en klass flyta runt i huvudprogrammet
             System.out.println("Denna växt ska ha 0,2 cl mineralvatten");
